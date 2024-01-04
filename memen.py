e21aa364596b538aa197d1f6269bbc12b2562e4e
Jose Valverde <josevalverde@Jose-Valverde-Hakkoda-C02G9AQNML7H.local>
2024-01-04 09:30:20 -0600

    first commit

---
+++ memen.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def main():
+    num = int(input("Enter a number to calculate its factorial: "))
+    result = factorial(num)
+    print(f"The factorial of {num} is: {result}")
+
+if __name__ == "__main__":
+    main()
